04b95f75c64c793df3b51f7ff13ea3834a3f9917
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-02-26 10:52:37 +0100

    Added Category and Customer Mappers, CustomerDTO

--- src/main/java/guru/springframework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springframework/api/v1/model/CustomerDTO.java
@@ -1,10 +1,13 @@
 package guru.springframework.api.v1.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
     private String firstName;
     private String lastName;
+
+    @JsonProperty("customer_url")
     private String customerUrl;
 }
