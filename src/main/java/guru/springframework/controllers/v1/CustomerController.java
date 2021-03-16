0d4f61710a8659f97013431499de74be9888e0f6
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-03-17 00:49:39 +0100

    Added Contact info to Swagger page.

--- src/main/java/guru/springframework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springframework/controllers/v1/CustomerController.java
@@ -3,10 +3,13 @@ package guru.springframework.controllers.v1;
 import guru.springframework.api.v1.model.CustomerDTO;
 import guru.springframework.api.v1.model.CustomerListDTO;
 import guru.springframework.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description = "This is the Customer Controller")
 @RestController
 @RequestMapping("/api/v1/customers")
 public class CustomerController {
@@ -19,6 +22,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "This will give a list of customers", notes = "There are some notes about API")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomerListDTO getListOfCustomers(){
